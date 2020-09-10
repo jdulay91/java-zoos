8bd5e61071ba0b75eddb70066e0e83fd3265f331
Julliann Dulay <jullianndulay@gmail.com>
2020-09-10 16:54:13 -0700

    first query working

--- src/main/java/com/lambdaschool/zoos/services/AnimalServicesImplementation.java
+++ src/main/java/com/lambdaschool/zoos/services/AnimalServicesImplementation.java
@@ -1,10 +1,23 @@
 package com.lambdaschool.zoos.services;
 
 import com.lambdaschool.zoos.repository.AnimalRepository;
+import com.lambdaschool.zoos.views.ZooCounts;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional
+@Service(value = "animalServices")
 public class AnimalServicesImplementation implements AnimalServices {
 
     @Autowired
     private AnimalRepository animalrepos;
+
+    @Override
+    public List<ZooCounts> getZooCounts() {
+        List<ZooCounts> list = animalrepos.findZooCounts();
+        return list;
+    }
 }
